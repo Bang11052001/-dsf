fe9e03cd6c737c6f19e86f45fa6841860806bc65
quanganh <khanhbang.work@gmail.com>
2022-06-05 23:43:56 +0700

    upload camon.php

---
+++ web/pages/main/camon.php
@@ -0,0 +1,6 @@
+<div class="dangky" style ="margin-Top:100px">
+<div class="container">
+<p>Cám ơn bạn đã mua hàng ,chúng tôi sẽ liên hệ bạn trong thời gian sớm nhất</p>
+<a href="index.php">Tiếp tục mua sắm</a>
+</div>
+</div>
