0fd743bd63f7222e4fbe42002c4a92216d09e693
hoangkyanh <khanhbang3.work@gmail.com>
2022-06-14 23:37:50 +0700

    upload file quanlyslide/them.php

---
+++ web/admin/modules/quanlyslide/them.php
@@ -0,0 +1,15 @@
+<p>Thêm sản phẩm</p>
+<table border="1" width="100%" style="border-collapse: collapse;">
+ <form method="POST" action="modules/quanlyslide/xuly.php" enctype="multipart/form-data">
+	
+	   <tr>
+	  	<td>Hình ảnh </td>
+	  	<td><input type="file" name="hinh"></td>
+	  </tr>
+	  
+	
+	   <tr>
+	    <td colspan="2"><input type="submit" name="themslide" value="Thêm "></td>
+	  </tr>
+ </form>
+</table>
