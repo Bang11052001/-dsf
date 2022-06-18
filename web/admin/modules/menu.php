182f9e5bba3328bfd03ee7f9c514e55eb1c9df37
hoangkyanh <khanhbang3.work@gmail.com>
2022-06-19 04:54:19 +0700

    upload modules/menu.php

---
+++ web/admin/modules/menu.php
@@ -0,0 +1,34 @@
+<div class="web-menu">
+    <p>Slide ảnh</p>
+    <ul>
+        <li><a href="index.php?action=tsxslideanh&query=them">Thêm, sửa, xóa slide ảnh</a></li>
+        <li><a href="index.php?action=danhsachslide&query=them">Danh sách sldie ảnh </a></li>
+    </ul>
+    <p>Danh mục</p>
+    <ul>
+        <li><a href="index.php?action=tsxdanhmuc&query=them">Thêm, sửa, xóa danh mục danh mục</a></li>
+        <li><a href="index.php?action=danhsachdanhmuc&query=them">Danh sách danh mục </a></li>
+    </ul>
+    <p>Sản phẩm</p>
+    <ul>
+        <li><a href="index.php?action=tsxsanpham&query=them">Thêm, sửa, xóa sản phẩm</a></li>
+        <li><a href="index.php?action=danhsachsanpham&query=them">Danh sách sản phẩm</a></li>
+        <li><a href="index.php?action=danhsachsanpham&query=them">Thống kê sản phẩm</a></li>
+    </ul>
+    <p>Đơn hàng</p>
+    <ul>
+        <li><a href="index.php?action=quanlydonhang&query=lietke">Danh sách đơn hàng</a></li>
+       
+    </ul>
+    <p>Tài khoản người dùng</p>
+    <ul>
+        <li><a href="index.php?action=quanlytk&query=them">Danh sách người dùng </a></li>
+       
+    </ul>
+    <p>Thống kê báo cáo</p>
+    <ul>
+        <li><a href="index.php?action=thongke&query=them">Thông kê </a></li>
+       
+    </ul>
+    
+</div>
